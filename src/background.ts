7477898b2e7b19be783e7b819711bbe6e42dd7c2
nagaogn <idomagine@gmail.com>
2023-07-03 14:18:02 +0900

    Refactor: Remove unnecessary commentout in background.ts

--- src/background.ts
+++ src/background.ts
@@ -19,20 +19,3 @@ chrome.runtime.onInstalled.addListener((details) => {
 		setOptions(options);
 	}
 });
-
-
-// chrome.storage.onChanged.addListener((changes, namespace) => {
-// 	for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-// 		console.log(
-// 			`Storage key "${key}" in namespace "${namespace}" changed.`,
-// 			`Old value was "${oldValue}", new value is "${newValue}".`
-// 		);
-// 	}
-// });
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-// 	console.log("created tabs and update");
-// 	if (changeInfo.status === 'complete') {
-// 		console.log("status is complete");
-// 	}
-// });
